bc2897860343de47fc17152af87be7953a9daa70
LongXiangL <bfh1104@gmail.com>
2023-03-04 16:54:45 +0800

    feat:模板調整，加入增加餐廳功能

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 const express = require('express')
-const Restaurant = require('./models/restaurant') // 載入 restaurant model
+const exphbs = require('express-handlebars')
 const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
-const exphbs = require('express-handlebars');
+const Restaurant = require('./models/restaurant') // 載入 restaurant model
+const bodyParser = require('body-parser')// 引用 body-parser
 
-
-
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+// setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+app.use(express.static("public"))//使用public中的css設定
+app.set('view engine', 'hbs')// 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
+
 // 取得資料庫連線狀態
 const db = mongoose.connection
-// 連線異常
 db.on('error', () => {
-  console.log('mongodb error!')
+  console.log('mongodb error!')// 連線異常
 })
-// 連線成功
 db.once('open', () => {
-  console.log('mongodb connected!')
+  console.log('mongodb connected!')// 連線成功
 })
+
 
 //設定路由
 
@@ -35,6 +37,18 @@ app.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
+//新增餐廳頁面
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+//新增餐廳
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Restaurant.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
 //設定port
 app.listen(3000, () => {
   console.log('app is running on http://localhost3000')
